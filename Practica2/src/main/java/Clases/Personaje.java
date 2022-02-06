5be90d68cfa48843c6ad373c65ef2b4bc1e1a409
iAdrian14 <58402454+iAdrian14@users.noreply.github.com>
2022-02-05 19:05:56 -0600

    progreso

--- Practica2/src/main/java/Clases/Personaje.java
+++ Practica2/src/main/java/Clases/Personaje.java
@@ -8,6 +8,47 @@ package Clases;
  *
  * @author Leonidas Adrian Mendoza Flores
  */
+import java.util.Random;
 public class Personaje {
+    
+    public void setNombre(String name)
+    {
+        this.nombre = name;
+    }
+    public void setSalud(int hp)
+    {
+        this.salud = hp;
+    }
+    public void setAtaque(int atk)
+    {
+        this.ataque = atk;
+    }
+    public void setDefensa(int def)
+    {
+        this.defensa = def;
+    }
+    //Tiene que ser del 1 al 100
+    public void setPrecision(double acc)
+    {
+        this.precision = acc;
+    }
+    public void atacar(int eSalud, int eDefensa)
+    {
+        Random r = new Random();
+        double acierto = 1 + (99 + 1) * r.nextDouble();
+        if(this.precision > acierto)
+        {
+            int daño = eSalud - this.ataque/eDefensa;
+        }
+        else
+        {
+            System.out.println("Fallo");
+        }
+    }
+    private String nombre = "";
+    private int ataque = 0;
+    private int salud = 0;
+    private int defensa = 0;
+    private double precision = 1;
     
 }
